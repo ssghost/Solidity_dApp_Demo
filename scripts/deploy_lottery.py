76720b9bc6ac307dd252be55fd8257cce10e489b
ssghost <ssprof0@gmail.com>
2022-03-28 23:26:41 +0800

    add deploy

--- scripts/deploy_lottery.py
+++ scripts/deploy_lottery.py
@@ -1,8 +1,19 @@
-from brownie import accounts, config, network, Lottery
+from brownie import accounts, config, network, Lottery, Contract
+from brownie import MockV3Aggregator, VRFCoordinatorMock, LinkToken
+from scripts.deploy_mocks import deploy_mocks
 
 def deploy_lottery():
     account = get_account()
-    lottery = Lottery.deploy()
+    lottery = Lottery.deploy(
+        get_contract('price_feed').address,
+        get_contract('vrf_coordinator').address,
+        get_contract('link_token').address,
+        config['networks'][network.show_active()]['keyhash'],
+        config['networks'][network.show_active()]['vrffee'],
+        {'from': account},
+        publish_source=config['networks'][network.show_active()].get('verify', False),
+    )
+    return lottery
 
 def get_account(index=None, id=None):
     if index:
@@ -15,7 +26,15 @@ def get_account(index=None, id=None):
 
 def get_contract(c_name):
     name2mock = {'price_feed': MockV3Aggregator, 'vrf_coordinator': VRFCoordinatorMock, 'link_token': LinkToken}
-    pass
+    c_type = name2mock[c_name]
+    if network.show_active() in ['development', 'ganache-local']:
+        if len(c_type) <= 0:
+            deploy_mocks()
+        contract = c_type[-1]
+    else:
+        c_address = config['networks'][network.show_active()][c_name]
+        contract = Contract.from_abi(c_type._name, c_address, c_type.abi)
+    return contract
 
 
 def main():
--- scripts/deploy_mocks.py
+++ scripts/deploy_mocks.py
@@ -3,6 +3,6 @@ from scripts.deploy_lottery import get_account
 
 def deploy_mocks(decimals=8, initial_value=2*10**12):
     account = get_account()
-    MockV3Aggregator.deploy(decimals, initial_value, {"from": account})
-    link_token = LinkToken.deploy({"from": account})
-    VRFCoordinatorMock.deploy(link_token.address, {"from": account})
+    MockV3Aggregator.deploy(decimals, initial_value, {'from': account})
+    link_token = LinkToken.deploy({'from': account})
+    VRFCoordinatorMock.deploy(link_token.address, {'from': account})
